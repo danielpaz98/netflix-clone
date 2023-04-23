eeb0fe128f07650a2da83ce512d042fc95933a4e
Daniel Alexander Paz <alexdaniel-1998@outlook.com>
2023-04-23 12:50:16 -0500

    feat: add logout button to home page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,25 @@
+import { signOut } from "next-auth/react";
 // LAYOUTS
 import { DefaultLayout } from "~/layouts";
+// LIBRARIES
+import { withAuth } from "~/lib";
 
 const HomePage: NextPageWithLayout = () => {
-  return <h1>Netflix Clone</h1>;
+  return (
+    <>
+      <h1>Netflix Clone</h1>
+      <button type="button" onClick={() => signOut()}>
+        Logout
+      </button>
+    </>
+  );
 };
+
+export const getServerSideProps = withAuth(() => {
+  return {
+    props: {},
+  };
+});
 
 HomePage.title = "Inicio";
 HomePage.layout = DefaultLayout;
