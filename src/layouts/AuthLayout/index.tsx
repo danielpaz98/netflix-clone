781f1a7ed58b379d82bc8abed0df44b9134ca11a
Daniel Alexander Paz <alexdaniel-1998@outlook.com>
2023-04-23 12:39:00 -0500

    feat: add AuthLayout

--- src/layouts/AuthLayout/index.tsx
+++ src/layouts/AuthLayout/index.tsx
@@ -1,5 +1,26 @@
+// PLUGINS
+import Image from "next/image";
+// STYLES
+import Styles from "./styles";
+// IMAGES
+import BackgroundImage from "~/images/jpg/hero.jpg";
+
 const AuthLayout = ({ children }: { children: React.ReactNode }) => {
-  return <main>{children}</main>;
+  return (
+    <Styles.Container>
+      <Styles.Picture>
+        <Image alt="" src={BackgroundImage} />
+      </Styles.Picture>
+
+      <Styles.Header>
+        <Styles.HeaderLink href="/">
+          <Styles.HeaderLogo />
+        </Styles.HeaderLink>
+      </Styles.Header>
+
+      <Styles.Main>{children}</Styles.Main>
+    </Styles.Container>
+  );
 };
 
 export const Layout = (page: React.ReactElement) => <AuthLayout>{page}</AuthLayout>;
---
+++ src/layouts/AuthLayout/styles.ts
@@ -0,0 +1,63 @@
+import styled from "styled-components";
+// PLUGINS
+import Link from "next/link";
+// IMAGES
+import LogoImage from "~/images/svg/logo.svg";
+
+const Styles = {
+  Container: styled.div`
+    background-color: ${({ theme }) => theme.colors.black};
+    color: ${({ theme }) => theme.colors.white};
+    min-height: 100vh;
+    position: relative;
+    z-index: 0;
+    --header-height: 5.625rem;
+  `,
+  Picture: styled.picture`
+    display: none;
+    background-size: cover;
+    width: 100%;
+    height: 100%;
+    min-height: 100vh;
+    overflow: hidden;
+    position: absolute;
+    z-index: -1;
+    opacity: 0.5;
+
+    & img {
+      min-width: 100%;
+      min-height: 100%;
+      max-width: none;
+    }
+
+    @media screen and (min-width: ${({ theme }) => theme.breakpoints.sm}) {
+      display: block;
+    }
+  `,
+  Header: styled.header`
+    padding: 1.5rem;
+    padding-left: 5%;
+    height: var(--header-height);
+
+    @media screen and (min-width: ${({ theme }) => theme.breakpoints.sm}) {
+      background: linear-gradient(180deg, rgba(0, 0, 0, 0.5) 0, transparent);
+      padding-left: 3%;
+    }
+  `,
+  HeaderLink: styled(Link)`
+    color: ${({ theme }) => theme.colors.brand};
+  `,
+  HeaderLogo: styled(LogoImage)`
+    display: inline-block;
+    width: 10.5rem;
+  `,
+  Main: styled.main`
+    min-height: calc(100vh - var(--header-height));
+    display: flex;
+    flex-direction: column;
+    justify-self: center;
+    align-items: center;
+  `,
+};
+
+export default Styles;
