96b3c1ba42624b65fea049fe9dcf5594dfada181
Daniel Alexander Paz <alexdaniel-1998@outlook.com>
2023-04-23 12:49:39 -0500

    feat: add signin page for login

---
+++ src/pages/auth/signin/index.tsx
@@ -0,0 +1,60 @@
+import { useEffect, useRef } from "react";
+// PLUGINS
+import Link from "next/link";
+import { signIn } from "next-auth/react";
+// STYLES
+import Styles from "~/components/AuthForm/styles";
+// LAYOUTS
+import { AuthLayout } from "~/layouts";
+// COMPONENTS
+import AuthForm from "~/components/AuthForm";
+// ICONS
+import GoogleIcon from "~/icons/google.svg";
+import GithubIcon from "~/icons/github.svg";
+import ThreeDotsIcon from "~/icons/three-dots.svg";
+// TYPES
+import type { UseFormReturn } from "react-hook-form";
+// SCHEMAS
+import { signInSchema, type SignInSchema } from "~/components/AuthForm/schemas";
+// HOOKS
+import { useAuthForm } from "~/components/AuthForm/hooks";
+
+const SignInPage: NextPageWithLayout = () => {
+  const { login, isLoading, errors } = useAuthForm<SignInSchema>();
+  const formRef = useRef<UseFormReturn<SignInSchema>>(null);
+
+  useEffect(() => {
+    if (errors) formRef.current?.setError(...errors);
+  }, [errors]);
+
+  return (
+    <AuthForm ref={formRef} mode="onChange" resolver={signInSchema} onSubmit={login}>
+      <AuthForm.Title>Sign In</AuthForm.Title>
+
+      <AuthForm.Input label="Email" name="email" type="text" />
+      <AuthForm.Input label="Password" name="password" type="password" />
+
+      <AuthForm.Button disabled={isLoading} type="submit">
+        {isLoading ? <ThreeDotsIcon height={14} /> : <span>Login</span>}
+      </AuthForm.Button>
+
+      <AuthForm.ProviderList>
+        <AuthForm.ProviderList.Item onClick={() => signIn("google", { callbackUrl: "/" })}>
+          <GoogleIcon height={28} width={28} />
+        </AuthForm.ProviderList.Item>
+
+        <AuthForm.ProviderList.Item onClick={() => signIn("github", { callbackUrl: "/" })}>
+          <GithubIcon height={28} width={28} />
+        </AuthForm.ProviderList.Item>
+      </AuthForm.ProviderList>
+
+      <Styles.MutedLink>
+        <span>First time on Netflix?</span> <Link href="/auth/signup">Create an account.</Link>
+      </Styles.MutedLink>
+    </AuthForm>
+  );
+};
+
+SignInPage.layout = AuthLayout;
+
+export default SignInPage;
