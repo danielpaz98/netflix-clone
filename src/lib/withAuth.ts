cd410b890bad919521b7da05fd87991f76e8de74
Daniel Alexander Paz <alexdaniel-1998@outlook.com>
2023-04-26 11:15:00 -0500

    feat: return auth user for the withAuth HOC

--- src/lib/withAuth.ts
+++ src/lib/withAuth.ts
@@ -1,10 +1,13 @@
-// LIBRARIES
+// PLUGINS
 import { getSession } from "next-auth/react";
 // TYPES
 import type { Redirect, GetServerSidePropsResult, GetServerSidePropsContext } from "next";
 
-type GsspResult = GetServerSidePropsResult<{ [key: string]: unknown }>;
-type Gssp = (ctx: GetServerSidePropsContext) => Promise<GsspResult> | GsspResult;
+type Props = { [key: string]: unknown };
+
+type Gssp<P extends Props = Props> = (
+  ctx: GetServerSidePropsContext
+) => Promise<GetServerSidePropsResult<P>> | GetServerSidePropsResult<P>;
 
 function withAuth(gssp: Gssp, { redirect }: { redirect?: Redirect } = {}) {
   return async (ctx: GetServerSidePropsContext) => {
@@ -13,9 +16,14 @@ function withAuth(gssp: Gssp, { redirect }: { redirect?: Redirect } = {}) {
 
     if (!user) return redirectTo;
 
-    const gsspData = await gssp(ctx);
+    const gsspData = (await gssp(ctx)) || {};
 
-    return gsspData;
+    if (!("props" in gsspData)) throw new Error("invalid getSSP result");
+
+    const { props, ...restGssp } = gsspData;
+    const gsspReturn = { ...restGssp, props: { user, ...(props as Props) } };
+
+    return gsspReturn;
   };
 }
 
