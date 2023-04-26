02522e1fce9b67defe96ec7be3dead6bd65871d2
Daniel Alexander Paz <alexdaniel-1998@outlook.com>
2023-04-26 11:08:55 -0500

    feat: add generic Props for the type NextPageWithLayout

--- src/types/layout.d.ts
+++ src/types/layout.d.ts
@@ -3,7 +3,7 @@ import type { NextPage } from "next";
 import type { AppProps } from "next/app";
 
 declare global {
-  type NextPageWithLayout = NextPage & {
+  type NextPageWithLayout<Props = {}> = NextPage<Props> & {
     title?: string;
     layout?: (page: React.ReactElement) => JSX.Element;
   };
