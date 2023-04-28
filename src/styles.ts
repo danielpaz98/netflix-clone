150989847ec0a7ce5dd197d362ec44776f6080cf
Daniel Alexander Paz <alexdaniel-1998@outlook.com>
2023-04-28 11:28:05 -0500

    refactor: remove unnused styles

--- src/styles.ts
+++
@@ -1,9 +0,0 @@
-import styled from "styled-components";
-
-const Styles = {
-  Button: styled.button`
-    background-color: ${({ theme }) => theme.colors.brand};
-  `,
-};
-
-export default Styles;
