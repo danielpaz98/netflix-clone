d41df6daa9d93e50e92d86683b2587ff83033b3d
Daniel Alexander Paz <alexdaniel-1998@outlook.com>
2023-04-30 01:49:57 -0500

    feat: add forwardRef to the Input component

--- src/components/Input/index.tsx
+++ src/components/Input/index.tsx
@@ -1,3 +1,4 @@
+import { forwardRef } from "react";
 // STYLES
 import Styles from "./styles.css";
 
@@ -5,28 +6,36 @@ export interface Props extends React.InputHTMLAttributes<HTMLInputElement> {
   label: string;
 }
 
-const Input = ({ className, label, name, placeholder, type, onChange, ...restProps }: Props) => {
-  const floatLabel = Boolean(label && !placeholder);
+const Input = forwardRef(
+  (
+    { className, label, name, placeholder, type, onChange, ...restProps }: Props,
+    ref?: React.Ref<HTMLInputElement>
+  ) => {
+    const floatLabel = Boolean(label && !placeholder);
 
-  return (
-    <Styles.Container className={className}>
-      <Styles.Input
-        floatLabel={floatLabel}
-        id={name}
-        name={name}
-        placeholder={placeholder ?? " "}
-        type={type}
-        onChange={onChange}
-        {...restProps}
-      />
+    return (
+      <Styles.Container className={className}>
+        <Styles.Input
+          ref={ref}
+          floatLabel={floatLabel}
+          id={name}
+          name={name}
+          placeholder={placeholder ?? " "}
+          type={type}
+          onChange={onChange}
+          {...restProps}
+        />
 
-      {label && (
-        <Styles.Label floatLabel={floatLabel} htmlFor={name}>
-          {label}
-        </Styles.Label>
-      )}
-    </Styles.Container>
-  );
-};
+        {label && (
+          <Styles.Label floatLabel={floatLabel} htmlFor={name}>
+            {label}
+          </Styles.Label>
+        )}
+      </Styles.Container>
+    );
+  }
+);
+
+Input.displayName = "Input";
 
 export default Input;
