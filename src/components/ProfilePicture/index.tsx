5817f5ba14d43b0f0d7a27682bd01931aedc509f
Daniel Alexander Paz <alexdaniel-1998@outlook.com>
2023-05-07 19:40:00 -0500

    refactor: remove DefaultImage

--- src/components/ProfilePicture/index.tsx
+++ src/components/ProfilePicture/index.tsx
@@ -2,8 +2,6 @@
 import Image from "next/image";
 // STYLES
 import Styles from "./styles.css";
-// IMAGES
-import DefaultImage from "~/images/png/default-blue.png";
 
 interface Props extends React.ComponentProps<"figure"> {
   className?: string;
@@ -15,14 +13,16 @@ const ProfilePicture = ({ className, image, username, ...restProps }: Props) => 
   return (
     <Styles.Figure className={className} {...restProps}>
       <Styles.Picture>
-        <Image
-          fill
-          priority
-          alt={(username as string) || ""}
-          sizes="(max-width: 640px) 100vw, (max-width: 1024px) 50vw, 33vw"
-          src={image || DefaultImage}
-          style={{ objectFit: "cover", objectPosition: "center" }}
-        />
+        {image && (
+          <Image
+            fill
+            priority
+            alt={(username as string) || ""}
+            sizes="(max-width: 640px) 100vw, (max-width: 1024px) 50vw, 33vw"
+            src={image}
+            style={{ objectFit: "cover", objectPosition: "center" }}
+          />
+        )}
       </Styles.Picture>
 
       {username && <Styles.Text>{username}</Styles.Text>}
