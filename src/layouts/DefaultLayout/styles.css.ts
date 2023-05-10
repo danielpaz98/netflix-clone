f32b0916ab2fd1b388a2e6feb6d6771a61fabe1d
Daniel Alexander Paz <alexdaniel-1998@outlook.com>
2023-05-10 12:00:59 -0500

    fix: overflow of content page

--- src/layouts/DefaultLayout/styles.css.ts
+++ src/layouts/DefaultLayout/styles.css.ts
@@ -4,10 +4,6 @@ const Styles = {
   Container: styled.div`
     background-color: ${({ theme }) => theme.colors.nero};
     color: ${({ theme }) => theme.colors.white};
-    height: 100%;
-  `,
-  Main: styled.main`
-    height: 100%;
   `,
 };
 
