c1d069313f5340f86d0b7b0b76492ae2f2af44e6
Daniel Alexander Paz <alexdaniel-1998@outlook.com>
2023-05-12 14:42:52 -0500

    style: format code with prettier

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -4,8 +4,14 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head>
-        <link href="https://assets.nflxext.com/us/ffe/siteui/common/icons/nficon2016.ico" rel="shortcut icon" />
-        <link href="https://assets.nflxext.com/us/ffe/siteui/common/icons/nficon2016.png" rel="apple-touch-icon" />
+        <link
+          href="https://assets.nflxext.com/us/ffe/siteui/common/icons/nficon2016.ico"
+          rel="shortcut icon"
+        />
+        <link
+          href="https://assets.nflxext.com/us/ffe/siteui/common/icons/nficon2016.png"
+          rel="apple-touch-icon"
+        />
         <link href="https://necolas.github.io/normalize.css/8.0.1/normalize.css" rel="stylesheet" />
       </Head>
 
